e1eee3876ca612e7bfe665c6bff4b5cb43a5f222
Matiaguilar <you@example.com>
2021-12-08 19:04:44 -0300

    agregadas las ods

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Ods(models.Model):
 	nombre= models.CharField(max_length = 30)
 	descripcion= models.CharField(max_length = 100, null = False)
+	imagen= models.ImageField(upload_to='imagenes_ods', null=True)
 
 	def __str__(self):
 		return self.nombre
