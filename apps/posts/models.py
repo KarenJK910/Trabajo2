83ee7a67ee5916a5c9239bd6c0b1729de38445ab
Matiaguilar <you@example.com>
2021-12-08 17:01:51 -0300

    segundaprueba

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Ods(models.Model):
+	nombre= models.CharField(max_length = 30)
+	descripcion= models.CharField(max_length = 100, null = False)
+
+	def __str__(self):
+		return self.nombre
+
+class Posts(models.Model):
+	Titulo= models.CharField(max_length = 30)
+	descripcion= models.CharField(max_length = 100, null = False)
+	Ods=models.ForeignKey(Ods, on_delete = models.CASCADE)
+	imagen= models.ImageField(upload_to='imagenes_posts', null=True)
+
+	def __str__(self):
+		return self.Titulo
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
+from .models import Ods, Posts
 
 # Create your views here.
 def Posts_Destacados(request):
-	return render (request,'posts/Posts_Destacados.html')
+	p = Posts.objects.all()
+	ctx= {}
+	ctx['Posts'] = p
+	ctx['titulo'] = 'Hola soy el titulo'
+
+	return render (request,'Base.html',ctx)
+
+
+
+
+'''
+# Create your views here.
+def Listar_Productos(request):
+
+	#consulta para traer los productos.
+	p = Producto.objects.all()  #select * from productos
+	#para mandarlo al template se usa un contexto
+
+	ctx= {}
+	ctx['product'] = p
+	ctx['titulo'] = 'Hola soy el titulo'
+
+	return render (request,'producto/listar_producto.html',ctx)
+
+
+def DetalleProducto(request,pk):
+	return render(request,productos/DetalleProducto.html)
+
+def DetalleProducto(request,pk):
 
 
 # Create your views here.
+'''
